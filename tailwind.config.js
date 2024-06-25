eec895932461ca8d797afe197a750999b4bf2fa5
Dencel K Babu <dencelbabu@gmail.com>
2024-06-25 17:13:01 -0400

    ops: update tailwind config
    Signed-off-by: Dencel K Babu <dencelbabu@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./**/*.html", "./src/**/*.{js,jsx,ts,tsx}"
+  ],
   theme: {
     extend: {},
   },
