97756da7be00d392bd1b654d9d1be428c533fcbc
Dencel K Babu <dencelbabu@gmail.com>
2024-06-26 00:30:55 -0400

    refactor: intial prettier commit
    Signed-off-by: Dencel K Babu <dencelbabu@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: [
-    "./**/*.html", "./src/**/*.{js,jsx,ts,tsx}"
-  ],
+  content: ["./**/*.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [
-    require("@tailwindcss/typography"),
-    require('daisyui'),
-  ],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
 
   // daisyui settings:
-}
+};
