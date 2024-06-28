9ad856d17cedbbfbab45dbb4607662a773f510fb
Dencel K Babu <dencelbabu@gmail.com>
2024-06-28 01:42:04 -0400

    feat: initial homepage design
    Signed-off-by: Dencel K Babu <dencelbabu@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 
 const defaultTheme = require("tailwindcss/defaultTheme");
-
 module.exports = {
   content: ["./**/*.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        figtree: ['"Figtree"', ...defaultTheme.fontFamily.sans]
+        figtree: ['"Figtree"', ...defaultTheme.fontFamily.sans],
+        inter: ['"Inter"', ...defaultTheme.fontFamily.sans],
+        outfit: ['"Outfit"', ...defaultTheme.fontFamily.sans]
       }
     },
   },
-  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  plugins: [
+    require("@tailwindcss/typography")
+  ],
 
   // daisyui settings:
+  /*plugins: [
+    require("daisyui")
+  ]*/
 };
